ecad3c395cfd58f47d9990d4d80019a9c625a196
Klaudia Gadzińska <klaudiaaolczyk@gmail.com>
2025-07-08 10:12:53 +0200

    feat: app logo

---
+++ src/components/layout/logo.tsx
@@ -0,0 +1,9 @@
+import type { ReactNode } from 'react';
+
+export function Logo(): ReactNode {
+  return (
+    <span className="font-black text-primary px-2 py-1 rounded tracking-tighter text-lg">
+      isCloudy
+    </span>
+  );
+}
--- src/components/layout/root.tsx
+++ src/components/layout/root.tsx
@@ -1,10 +1,11 @@
 import { Link, Outlet } from 'react-router-dom';
+import { Logo } from '@/components/layout/logo';
 
 export const RootLayout = () => {
   return (
     <div>
-      <nav>
-        <Link to="/">Home</Link>
+      <nav className="py-2 px-1 shadow">
+        <Link to="/"><Logo/></Link>
       </nav>
       <main>
         <Outlet />
