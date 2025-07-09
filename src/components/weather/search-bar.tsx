8b4e487cc9bf164c95c63e307ba920fcc27b5e85
Klaudia Gadzińska <klaudiaaolczyk@gmail.com>
2025-07-09 14:53:59 +0200

    refactor: add zod resolver, search bar label and make it sticky to top of the page

--- src/components/weather/search-bar.tsx
+++ src/components/weather/search-bar.tsx
@@ -1,4 +1,5 @@
 import { type ReactNode } from 'react';
+import { zodResolver } from '@hookform/resolvers/zod';
 import { Input } from '@/components/ui/input';
 import { useNavigate } from 'react-router-dom';
 import { Button } from '@/components/ui/button';
@@ -9,37 +10,62 @@ import {
   FormDescription,
   FormField,
   FormItem,
+  FormLabel,
   FormMessage,
 } from '../ui/form';
+import z from 'zod';
+import { useAppDispatch } from '@/hooks/redux';
+import { resetWeather } from '@/store/weather';
 
 export default function SearchBar(): ReactNode {
   const navigate = useNavigate();
+  const dispatch = useAppDispatch();
+
   const form = useForm({
     defaultValues: {
       searchValue: '',
     },
+    resolver: zodResolver(
+      z.object({
+        searchValue: z.string().min(1, 'Wprowadź nazwę miasta lub kraju'),
+      }),
+    ),
   });
 
   const onSubmit = form.handleSubmit((data) => {
+    // clear previous weather data
+    dispatch(resetWeather());
     const encodedSearch = encodeURIComponent(data.searchValue.trim());
+    // navigate to the search page with encoded search value
     navigate(`/${encodedSearch}`);
   });
 
   return (
     <FormProvider {...form}>
-      <form onSubmit={onSubmit} className="w-full max-w-md flex gap-2 py-4 mt-4">
+      <form
+        onSubmit={onSubmit}
+        className="sticky top-0 z-20 w-full backdrop-blur flex-center gap-2 py-4"
+      >
         <FormField
           control={form.control}
           name="searchValue"
-          render={({ field }) => (
-            <FormItem className="grow">
+          render={({ field, fieldState }) => (
+            <FormItem className="max-w-md grow">
+              <FormLabel>Sprawdź pogodę</FormLabel>
               <FormControl>
-                <Input placeholder="Miasto lub kraj" {...field} />
+                <Input
+                  placeholder="Miasto lub kraj"
+                  className="bg-card"
+                  {...field}
+                />
               </FormControl>
-              <FormDescription className="text-xs">
-                Wprowadź nazwę miasta lub kraju, aby sprawdzić pogodę
-              </FormDescription>
-              <FormMessage />
+              {fieldState.error ? (
+                <FormMessage />
+              ) : (
+                <FormDescription>
+                  Wprowadź nazwę miasta lub kraju, aby sprawdzić pogodę
+                </FormDescription>
+              )}
             </FormItem>
           )}
         />
