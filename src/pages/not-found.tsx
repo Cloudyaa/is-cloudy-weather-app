12f84563e3269ce4360e2d83b71b590530aec18f
Klaudia Gadzińska <klaudiaaolczyk@gmail.com>
2025-07-09 12:50:57 +0200

    feat: 404 page

--- src/pages/not-found.tsx
+++ src/pages/not-found.tsx
@@ -1,10 +1,19 @@
 import { Link } from 'react-router-dom';
+import type { HTMLAttributes } from 'react';
+import { cn } from '@/lib/utils';
+import { Button } from '@/components/ui/button';
 
-export const NotFoundPage = () => {
+type NotFoundPageProps = HTMLAttributes<HTMLDivElement>;
+export const NotFoundPage = ({className, ...props}: NotFoundPageProps) => {
   return (
-    <div>
-      <h1>404 - Page Not Found</h1>
-      <Link to="/">Go back to home</Link>
+    <div className={cn('flex-center flex-col', className)} {...props}>
+      <h1 className="text-9xl">404</h1>
+      <h3>Nie znaleziono pogody dla wybranej lokalizacji.</h3>
+      <h5>Aplikacja używa języka polskiego. Spróbuj wprowadzić nazwę ponownie. </h5>
+      <p className="py-6">lub</p>
+      <Button asChild>
+        <Link to="/">Wróć do strony głównej</Link>
+      </Button>
     </div>
   );
 };
