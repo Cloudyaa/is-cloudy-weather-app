7df5b9c4bfc47509aaae0b40a2f88e6132ec389d
Klaudia Gadzińska <klaudiaaolczyk@gmail.com>
2025-07-09 14:53:00 +0200

    refactor: create fetchWeatherData function to fetch weather data

---
+++ src/lib/weather-fetch.ts
@@ -0,0 +1,36 @@
+import type { WeatherData } from '@/types/weather';
+
+const API_KEY = import.meta.env.VITE_OPENWEATHER_API_KEY;
+
+export async function fetchWeatherData(searchValue: string){
+  const res = await fetch(
+    `https://api.openweathermap.org/data/2.5/weather?q=${encodeURIComponent(searchValue)}&units=metric&appid=${API_KEY}&lang=pl`,
+  );
+  if (!res.ok) {
+    throw new Error(`Failed to fetch weather data for ${searchValue}`);
+  }
+
+  const data = await res.json();
+  return {
+    city: data.name,
+    country: data.sys.country,
+    lat: data.coord.lat,
+    lon: data.coord.lon,
+    main: data.weather[0].main,
+    description: data.weather[0].description,
+    temperature: {
+      main: data.main.temp.toFixed(0),
+      min: data.main.temp_min.toFixed(0),
+      max: data.main.temp_max.toFixed(0),
+      feelsLike: data.main.feels_like.toFixed(0),
+    },
+    sunrise: data.sys.sunrise,
+    sunset: data.sys.sunset,
+    clouds: data.clouds.all,
+    humidity: data.main.humidity,
+    windSpeed: data.wind.speed,
+    visibility: data.visibility,
+    pressure: data.main.pressure,
+    icon: data.weather[0].icon,
+  } as WeatherData;
+}
--- src/store/weather/weather.fetch.ts
+++ src/store/weather/weather.fetch.ts
@@ -1,43 +1,10 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
-import type { WeatherData } from '@/types/weather';
-
-const API_KEY = import.meta.env.VITE_OPENWEATHER_API_KEY;
+import { fetchWeatherData } from '@/lib/weather-fetch';
 
 /**
  * Fetch current weather data for specific city
  */
 export const fetchCurrentWeather = createAsyncThunk(
   'weather/fetchCurrentWeather',
-  async (city: string) => {
-    const res = await fetch(
-      `https://api.openweathermap.org/data/2.5/weather?q=${encodeURIComponent(city)}&units=metric&appid=${API_KEY}&lang=pl`,
-    );
-    if (!res.ok) {
-      throw new Error(`Failed to fetch weather data for ${city}`);
-    }
-
-    const data = await res.json();
-    return {
-      city: data.name,
-      country: data.sys.country,
-      lat: data.coord.lat,
-      lon: data.coord.lon,
-      main: data.weather[0].main,
-      description: data.weather[0].description,
-      temperature: {
-        main: data.main.temp.toFixed(0),
-        min: data.main.temp_min.toFixed(0),
-        max: data.main.temp_max.toFixed(0),
-        feelsLike: data.main.feels_like.toFixed(0),
-      },
-      sunrise: data.sys.sunrise,
-      sunset: data.sys.sunset,
-      clouds: data.clouds.all,
-      humidity: data.main.humidity,
-      windSpeed: data.wind.speed,
-      visibility: data.visibility,
-      pressure: data.main.pressure,
-      icon: data.weather[0].icon,
-    } as WeatherData;
-  },
+  async (city: string) => await fetchWeatherData(city),
 );
